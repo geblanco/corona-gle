5fe82292d788e58d4e5902580a1fab40a54d5863
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-04-06 10:20:38 +0200

    Finish parsing full outline structures

--- scrappers/elsevier/elsevier_process_outlines.py
+++ scrappers/elsevier/elsevier_process_outlines.py
@@ -1,98 +1,100 @@
-import requests
 import requests_random_user_agent
-import threading
-from pymongo import errors, MongoClient
+import concurrent.futures as cf
+import requests
 import config
 import json
-from progress.bar import Bar
 
+from tqdm import tqdm
+from pymongo import errors, MongoClient
+
+NUM_WORKERS = 8
 threads_amount = 5
 threads = list()
 
+"""
+sample
+{'#name': 'entry',
+ '$': {'id': 'sec3', 'type': 'sections', 'depth': '1'},
+ '$$': [{'#name': 'label', '_': '3'},
+  {'#name': 'title', '$': {'id': 'sectitle0065'}, '_': 'Results'},
+  {'#name': 'entry',
+   '$': {'id': 'sec3.1', 'depth': '2'},
+   '$$': [{'#name': 'label', '_': '3.1'},
+    {'#name': 'title',
+     '$': {'id': 'sectitle0070'},
+     '$$': [{'#name': '__text__',
+       '_': 'Alteration of miRNAs after IBV infection '},
+      {'#name': 'italic', '$': {'xmlns': True}, '_': 'in vivo'}]}]},
+  {'#name': 'entry',
+   '$': {'id': 'sec3.2', 'depth': '2'},
+   '$$': [{'#name': 'label', '_': '3.2'},
+    {'#name': 'title',
+     '$': {'id': 'sectitle0075'},
+     '_': 'Up-regulation of miR-146a-5p in cells infected with Beaudette'}]},
+  {'#name': 'entry',
+   '$': {'id': 'sec3.3', 'depth': '2'},
+   '$$': [{'#name': 'label', '_': '3.3'},
+    {'#name': 'title',
+     '$': {'id': 'sectitle0080'},
+     '_': 'miR-146a-5p related genes'}]}]}
+"""
 def get_title(text):
+    aux = ''
     if '_' in text.keys():
-        return(text['_'])
-    else:
-        aux = ''
+        aux = text['_']
+    elif text['$$']:
+        aux = []
         for part in text['$$']:
-            aux += part['_']
-        return(aux)
+            if '_' in part.keys():
+                aux.append(part['_'].strip())
+            elif part['#name'] == 'title':
+                # only one level down
+                aux.extend([p['_'].strip() for p in part['$$']])
+        aux = ' '.join(aux).strip()
+    return aux
 
-def process_entry(text, sections):
-    if text['#name'] == 'title':
-        sections.append(get_title(text))
-    elif text['#name'] == 'entry':
-        for elem in text['$$']:
-            sections = process_entry(elem, sections)
-            # res = process_entry(elem, [], 0)
-            # print('res:' + str(res))
-            # sections['subsections'] = res
-            # sections.append(aux)
-    # print('->' + str(sections) + '<-')
-    return(sections)
+def get_type(text):
+    ret = 'UNK'
+    if '$' in text.keys() and 'type' in text['$'].keys():
+        ret = text['$']['type']
+    return ret
+
+def process_entry(text):
+    section = {}
+    if text['#name'] == 'entry':
+        section['name'] = get_title(text)
+        section['type'] = get_type(text)
+        section['subsections'] = []
+        if '$$' in text.keys():
+            for entry in text['$$']:
+                subsection = process_entry(entry)
+                if subsection is not None:
+                    section['subsections'].append(subsection)
+    return section if len(section.keys()) > 0 else None
 
 def process_outline(json_outline):
     sections = []
-    for elem in json_outline:
-        if elem['$']['type'] == 'sections':
-            for sec in elem['$$']:
-                if sec['#name'] == 'title':
-                    sections.append(get_title(sec))
-                elif sec['#name'] == 'entry':
-                    for entry in sec['$$']:
-                        pass
-    print(sections)
-    return(sections)
+    for entry in json_outline:
+        sections.append(process_entry(entry))
+    return sections
 
-def process_outlines(bar, sem, db_client, col, sha):
-    '''
-    After the processing of the outline, we can have three states:
-    1. Checked = True; toc = None
-    2. Checked = True; toc = []
-    3. Checked = True; toc = [content]
+def process_raw_column(raw_column):
+    json_outline = json.loads(raw_column['raw'])['outline']
+    sections = process_outline(json_outline)
+    return { 'sha': raw_column['sha'], 'sections': sections }
 
-    The first state means that it has been process, but an error has been encountered; 
-    the second means that it has been correctly processed, but there are no content; 
-    and the last one means correct execution and available content
-    '''
-    with sem:
-        # bar.next()
-        db_papers = db_client[config.db_name]
-        col_to_work = db_papers[col]
-        sections = {}
-        try:
-            json_outline = json.loads(col_to_work.find_one({'sha':sha}, {'_id':0, 'raw':1})['raw'])['outline']
-            sections = process_outline(json_outline)
-            col_to_work.update_one({'sha':sha}, {'$set': {'checked':True, 'toc': sections}})
-        except Exception as e:
-            col_to_work.update_one({'sha':sha}, {'$set': {'checked':True, 'toc': None}})
-            raise(e) 
-        
-def thread_caller(sem, db_client, col):
-    db_papers = db_client[config.db_name]
-    col_to_work = db_papers[col]
+def main(client, database):
     filter_checked = {'checked': True}
-    documents = col_to_work.find(filter_checked, {'_id': 0, 'sha': 1})
-    bar = Bar('Processing', max=col_to_work.count_documents)(filter_checked)
-    for document in documents:
-        try:
-            t = threading.Thread(target=process_outlines, args=(bar, sem, db_client, col, document['sha']))
-            threads.append(t)
-            t.start()
-        except RuntimeError:
-            pass
-        except Exception as e:
-            raise(e)
-            # print('[0] error obtaining info from "{}"'.format(document['sha']))
-    for t in threads:
-        t.join()
-    bar.finish()
+    documents = database.find(filter_checked)
+    with cf.ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+        for sha, sections in tqdm(executor.map(process_raw_column, documents), total=len(documents)):
+            with Connection.CLIENT.start_session() as session:
+                with session.start_transaction():
+                    database.update_one({'sha': sha}, {'$set': {'checked':True, 'toc': sections}})
 
 if __name__ == "__main__":
     col_elsevier = config.collection_elsevier
     db_client = MongoClient(config.mongoURL)
-    sem = threading.Semaphore(threads_amount)
-    thread_caller(sem, db_client, col_elsevier)
-    # process_outlines(None, sem, db_client, col_elsevier, '28ef2f6aa0e0c14f92055d24d72b151fd4da910a')
+    db_papers = db_client[config.db_name]
+    main(db_client, db_papers[col_elsevier])
     db_client.close()
-
